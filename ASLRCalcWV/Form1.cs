6e3f9c1948c3624557fa146da79c45c6b62444b0
zeroKilo <b_d_b@web.de>
2020-01-13 19:56:18 +0100

    updated to 64 bit
    updated to 64 bit

--- ASLRCalcWV/Form1.cs
+++ ASLRCalcWV/Form1.cs
@@ -23,56 +23,63 @@ namespace ALSRCalc
 
         private void button1_Click(object sender, EventArgs e)
         {
-            list = Process.GetProcesses();
-            while (true)
+            try
             {
-                bool found = false;
-                for(int i = 0; i< list.Length -1;i++)
-                    if (list[i].ProcessName.CompareTo(list[i + 1].ProcessName) > 0)
-                    {
-                        Process p = list[i];
-                        list[i] = list[i + 1];
-                        list[i + 1] = p;
-                        found = true;
-                    }
-                if (!found)
-                    break;
+                list = Process.GetProcesses();
+                while (true)
+                {
+                    bool found = false;
+                    for (int i = 0; i < list.Length - 1; i++)
+                        if (list[i].ProcessName.CompareTo(list[i + 1].ProcessName) > 0)
+                        {
+                            Process p = list[i];
+                            list[i] = list[i + 1];
+                            list[i + 1] = p;
+                            found = true;
+                        }
+                    if (!found)
+                        break;
+                }
+                comboBox1.Items.Clear();
+                foreach (Process p in list)
+                    comboBox1.Items.Add(p.Id.ToString("D5") + " - " + p.ProcessName);
+                comboBox1.SelectedIndex = 0;
             }
-            comboBox1.Items.Clear();
-            foreach (Process p in list)
-                comboBox1.Items.Add(p.Id.ToString("D5") + " - " + p.ProcessName);
-            comboBox1.SelectedIndex = 0;
+            catch { textBox3.Text = "ERROR"; }
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            int n = comboBox1.SelectedIndex;
-            if (n == -1 || list == null || list.Length <= n)
-                return;
-            Process p = list[n];
-            comboBox2.Items.Clear();
-            modules = new List<ProcessModule>();
-            foreach (ProcessModule m in p.Modules)
-                modules.Add(m);
-            while (true)
+            try
             {
-                bool found = false;
-                for (int i = 0; i < modules.Count - 1; i++)
-                    if (modules[i].BaseAddress.ToInt64() > modules[i + 1].BaseAddress.ToInt64())
-                    {
-                        ProcessModule m = modules[i];
-                        modules[i] = modules[i + 1];
-                        modules[i + 1] = m;
-                        found = true;
-                    }
-                if (!found)
-                    break;
+                int n = comboBox1.SelectedIndex;
+                if (n == -1 || list == null || list.Length <= n)
+                    return;
+                Process p = list[n];
+                comboBox2.Items.Clear();
+                modules = new List<ProcessModule>();
+                foreach (ProcessModule m in p.Modules)
+                    modules.Add(m);
+                while (true)
+                {
+                    bool found = false;
+                    for (int i = 0; i < modules.Count - 1; i++)
+                        if (modules[i].BaseAddress.ToInt64() > modules[i + 1].BaseAddress.ToInt64())
+                        {
+                            ProcessModule m = modules[i];
+                            modules[i] = modules[i + 1];
+                            modules[i + 1] = m;
+                            found = true;
+                        }
+                    if (!found)
+                        break;
+                }
+                foreach (ProcessModule m in modules)
+                    comboBox2.Items.Add(m.BaseAddress.ToString("X8") + " - " + m.ModuleName);
+                if (comboBox2.Items.Count > 0)
+                    comboBox2.SelectedIndex = 0;
             }
-            foreach(ProcessModule m in modules)
-                comboBox2.Items.Add(m.BaseAddress.ToString("X8") + " - " + m.ModuleName);
-            if (comboBox2.Items.Count > 0)
-                comboBox2.SelectedIndex = 0;
-
+            catch { textBox3.Text = "ERROR"; }
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
